5998606c56c3908393e70f79b85903b6d17780ed
mikairyuu <>
2021-01-11 17:36:40 +1000

    начало работы над отслеживанием, дата объект

--- app/src/main/java/com/dvpro/kamen/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dvpro/kamen/ui/home/HomeFragment.kt
@@ -1,14 +1,18 @@
 package com.dvpro.kamen.ui.home
 
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.dvpro.kamen.R
+import java.util.prefs.Preferences
 
 class HomeFragment : Fragment() {
 
@@ -23,9 +27,18 @@ class HomeFragment : Fragment() {
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
+        val button : Button = root.findViewById(R.id.button)
+        button.setOnClickListener { } //TODO:Нажатие на кнопку
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             //textView.text = it
         })
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val sp = requireContext().getSharedPreferences("settings",MODE_PRIVATE);
+        sp.getInt("TrackingStatus",-1)
+
     }
 }
