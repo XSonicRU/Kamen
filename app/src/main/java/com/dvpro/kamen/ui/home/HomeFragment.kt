e8faa7776b393228f153947f13b0453b9a2f0251
mikairyuu <>
2021-01-12 11:13:25 +1000

    хотфикс инта

--- app/src/main/java/com/dvpro/kamen/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dvpro/kamen/ui/home/HomeFragment.kt
@@ -2,16 +2,20 @@ package com.dvpro.kamen.ui.home
 
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.dvpro.kamen.Data
 import com.dvpro.kamen.R
+import java.time.Instant
 import java.util.prefs.Preferences
 
 class HomeFragment : Fragment() {
@@ -27,8 +31,15 @@ class HomeFragment : Fragment() {
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        val button : Button = root.findViewById(R.id.button)
-        button.setOnClickListener { } //TODO:Нажатие на кнопку
+        val button: Button = root.findViewById(R.id.button)
+        button.setOnClickListener {
+            if (Data.TrackingStatus == -1L) {
+                button.text = resources.getText(R.string.button_value_on)
+                Data.TrackingStatus = System.currentTimeMillis()
+            } else {
+                button.text = resources.getText(R.string.button_value_off)
+            }
+        }
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             //textView.text = it
         })
@@ -37,8 +48,8 @@ class HomeFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        val sp = requireContext().getSharedPreferences("settings",MODE_PRIVATE);
-        sp.getInt("TrackingStatus",-1)
+        val sp = requireContext().getSharedPreferences("settings", MODE_PRIVATE);
+        sp.getInt("TrackingStatus", -1)
 
     }
 }
